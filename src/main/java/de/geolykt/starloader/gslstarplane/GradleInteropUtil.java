2ae90fd33d14aab0a0be12dfc66edb74aa87aab3
Geolykt <admin@geolykt.de>
2024-06-18 09:20:26 +0200

    Fix reflection logic in GradleInteropUtil
    I am not entirely sure why this is blowing up now of all times,
    but I assume it's because I'm using a different JDK than usual
    to invoke gradle with.

--- src/main/java/de/geolykt/starloader/gslstarplane/GradleInteropUtil.java
+++ src/main/java/de/geolykt/starloader/gslstarplane/GradleInteropUtil.java
@@ -3,6 +3,8 @@ package de.geolykt.starloader.gslstarplane;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import org.gradle.api.component.SoftwareComponent;
@@ -15,7 +17,7 @@ public class GradleInteropUtil {
     private static final MethodHandle COMPONENT_USAGE_CONTEXT_MH;
 
     @SuppressWarnings("unused") // Used reflectively via method handles
-    private static Iterable<UsageContext> onIncompatibleComponent(SoftwareComponent component) {
+    private static Set<UsageContext> onIncompatibleComponent(SoftwareComponent component) {
         throw new IllegalStateException("Only implementations of SoftwareComponent that are an instance of DefaultAdhocSoftwareComponent (gradle 7 and older gradle 8 releases) or SoftwareComponentInternal (newer gradle 8 releases) can be used as a mod jar. However an implementation of type " + component.getClass() + " was used!");
     }
 
@@ -41,15 +43,31 @@ public class GradleInteropUtil {
         MethodHandle softwareComponentMH = null;
         Class<?> softwareComponentImplClass = null;
 
-        try {
-            softwareComponentImplClass = lookup.findClass("org.gradle.api.plugins.internal.DefaultAdhocSoftwareComponent");
-        } catch (ReflectiveOperationException e) {
+        String[] softwareComponentImplClasses = new String[] {
+                "org.gradle.api.internal.component.SoftwareComponentInternal",
+                "org.gradle.api.publish.internal.component.SoftwareComponentInternal",
+                "org.gradle.api.publish.internal.component.DefaultAdhocSoftwareComponent",
+                "org.gradle.api.plugins.internal.DefaultAdhocSoftwareComponent"
+        };
+
+        List<ReflectiveOperationException> suppressed = null;
+        for (String softwareComponentImpl : softwareComponentImplClasses) {
             try {
-                softwareComponentImplClass = lookup.findClass("org.gradle.api.publish.internal.component.SoftwareComponentInternal");
-            } catch (ReflectiveOperationException e2) {
-                e2.addSuppressed(e);
-                LoggerFactory.getLogger(GradleInteropUtil.class).error("Unable to generate method handle for component content lookup. Some features might not work properly!", e2);
+                softwareComponentImplClass = lookup.findClass(softwareComponentImpl);
+                break;
+            } catch (ReflectiveOperationException e) {
+                if (suppressed == null) {
+                    suppressed = new ArrayList<>();
+                }
+                suppressed.add(e);
             }
+        }
+
+        if (softwareComponentImplClass == null && suppressed != null) {
+            ReflectiveOperationException lastException = suppressed.remove(suppressed.size() - 1);
+            suppressed.forEach(lastException::addSuppressed);
+            suppressed.clear();
+            LoggerFactory.getLogger(GradleInteropUtil.class).error("Unable to generate method handle for component content lookup. Some features might not work properly!", lastException);
         }
 
         label:
@@ -58,9 +76,9 @@ public class GradleInteropUtil {
                 break label;
             }
             MethodHandle isInstanceof = lookup.findVirtual(Class.class, "isInstance", MethodType.methodType(boolean.class, Object.class));
-            isInstanceof = isInstanceof.bindTo(softwareComponentImplClass);
+            isInstanceof = isInstanceof.bindTo(softwareComponentImplClass).asType(MethodType.methodType(boolean.class, softwareComponentImplClass));
             MethodHandle getUsages = lookup.findVirtual(softwareComponentImplClass, "getUsages", MethodType.methodType(Set.class));
-            MethodHandle throwEx = lookup.findStatic(GradleInteropUtil.class, "onIncompatibleComponent", MethodType.methodType(Iterable.class, SoftwareComponent.class));
+            MethodHandle throwEx = lookup.findStatic(GradleInteropUtil.class, "onIncompatibleComponent", MethodType.methodType(Set.class, SoftwareComponent.class)).asType(MethodType.methodType(Set.class, softwareComponentImplClass));
             softwareComponentMH = MethodHandles.guardWithTest(isInstanceof, getUsages, throwEx);
         } catch (ReflectiveOperationException e) {
             LoggerFactory.getLogger(GradleInteropUtil.class).error("Unable to generate method handle for component content lookup. Some features might not work properly!", e);
